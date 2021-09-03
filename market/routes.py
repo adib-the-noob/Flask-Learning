eabc16d60781cff928e9fca7fc62f34dbaac9ccb
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2021-09-03 19:34:21 +0600

    Project Restructure

---
+++ market/routes.py
@@ -0,0 +1,16 @@
+from market import app
+from flask import render_template
+from market.modules import Item 
+
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+
+@app.route("/market")
+def market():
+    items = Item.query.all()
+
+    return render_template('market.html', items=items)
