23b9d35c7c98cec4977b172fca7e562a6bb72a59
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2021-10-01 19:18:41 +0600

    Error Message with Box

--- market/routes.py
+++ market/routes.py
@@ -28,5 +28,5 @@ def register_page():
         return redirect(url_for('market'))
     if form.errors != {}:
         for  err_msg in form.errors.values():
-            flash(f'THere was an error with creating a user : {err_msg}')
+            flash(f'There was an error with creating a user : {err_msg}', category='danger')
     return render_template('register.html', form=form)
